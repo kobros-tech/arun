2f1c6c567b3f6e621d88c529a778f7c466da747d
kobros-tech <alkobroslymohamed@gmail.com>
2024-08-21 00:44:37 +0300

    first commit for stock

---
+++ custom_arun_stock/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    kobros-tech Pvt. Ltd.
+#
+#    Copyright (C) 2020-TODAY kobros-tech(<https://www.linkedin.com/company/kobros-tech/>).
+#    Author: Mohamed Alkobrosli(<https://www.linkedin.com/in/mohamed-alkobrosly/>)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+
+{
+    'name': "Custom inventory module Mr. Arun",
+    'description': """
+        Custom inventory module Mr. Arun
+    """,
+    'author': 'Abou Sajid (Mohamed Alkobrosli)',
+    'company': 'kobros-tech',
+    'maintainer': 'Mohamed Moustafa Alkobrosli',
+    'website': "https://www.kobros-tech.com",
+    'license': "AGPL-3",
+    'depends': [
+        'stock',
+        'custom_arun_sale',
+    ],
+    'data': [
+        "views/stock_picking_views.xml",
+        # "views/product_template_views.xml",
+
+        # "report/report_invoice.xml",
+        # "report/account_report.xml",
+    ],
+}
